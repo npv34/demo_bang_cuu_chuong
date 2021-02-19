30babe5e1429adaf53d336c708cbb9bb8b4abe38
npv34 <phanvanluan611996@gmail.com>
2021-02-19 20:55:30 +0700

    comment loop

--- my.js
+++ my.js
@@ -8,6 +8,16 @@ for (let i = 2; i < 10; i++) {
     }
     html += '</tr>';
 }
+// html = '';
+
+// + i = 2; 2<10 ->true ->
+    // html = '<tr>';
+    // j = 1; i < 10 -> true ->
+          // html = '<tr><td>';
+          // html = '<tr><td>2x1=2'
+          // html = '<tr><td>2x1=2</td>
+   // j = 2 ....
+
 
 document.getElementById('content').innerHTML = html
 
